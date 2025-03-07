da605142eb75c3039f699e5e1f0f9756efa00b2d
hanna-vk <hanna.molin@ivl.se>
2025-03-07 14:11:54 +0100

    Added files for exercise 1

---
+++ day_2/animals/.ipynb_checkpoints/fish-checkpoint.py
@@ -0,0 +1,9 @@
+class Fish:
+    def __init__(self):
+        ''' Constructor for this class. '''
+        # Create some member animals
+        self.members = ['Pike', 'Perch', 'Salmon']
+    def printMembers(self):
+        print('Printing members of the Fish class')
+        for member in self.members:
+            print('\t%s ' % member)
---
+++ day_2/animals/__init__.py
@@ -0,0 +1,6 @@
+from . import harmless
+from . import dangerous
+
+from .mammals import Mammals
+from .birds import Birds
+from .fish import Fish
